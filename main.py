48f903856a6837e91f0888cccb5938ba20fb50e5
Genndy <kochevgenazp@gmail.com>
2020-04-19 15:08:42 +0300

    Added so needed comments else i'd hate this code just tomorrow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ CELL_SIZE = 30
 MARGIN = 1
 CELLS_IN_ROW = 12
 FPS = 30
+# ---------------
 
 # COLOURS
 WHITE = (255, 255, 255)
@@ -18,11 +19,15 @@ YELLOW = (225, 225, 0)
 PINK = (230, 50, 230)
 # ----------------
 
+
+# Initiate and create a screen surface
 pygame.init()
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Pentagon")
-
+# Handling FPS or smth
 clock = pygame.time.Clock()
+
+
 #### CELLS PARTITION
 # Create all rects
 all_rects = [[(20 + (CELL_SIZE + MARGIN) * i, 20 + (CELL_SIZE + MARGIN) * j, CELL_SIZE, CELL_SIZE)
@@ -30,6 +35,8 @@ all_rects = [[(20 + (CELL_SIZE + MARGIN) * i, 20 + (CELL_SIZE + MARGIN) * j, CEL
 
 # All rects colours
 colours = [[WHITE for i in range(CELLS_IN_ROW)] for j in range(CELLS_IN_ROW)]
+# -------------------
+
 
 ##### FIGURES PARTITION
 # Create all figures start positions
@@ -47,23 +54,27 @@ fig_start_pos = {
     "STICK": [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)],
     "PLUS": [(1, 0), (1, 1), (0, 1), (1, 2), (2, 1)]
 }
+# ------------------------
 
 
 
-
-
+# Running game cycle
 running = True
 while running:
+    # Screen filler
     screen.fill(BLACK)
+    # Drawing a field
     for i in range(len(all_rects)):
         for j in range(len(all_rects[i])):
             pygame.draw.rect(screen, colours[i][j], all_rects[i][j])
-
+    # Drawing a figure
     for cell in fig_start_pos['PLUS']:
         colours[cell[0]][cell[1]] = GREEN
-
+    # Handling quit ability
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # Managing framerate
     clock.tick(FPS)
+    # Updating the whole picture
     pygame.display.update()
